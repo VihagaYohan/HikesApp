f695951e946dd265f14b437cdb7741d51d879097
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-10-11 08:52:52 +0530

    Initial Commit

---
+++ HikesApp/HikesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikesAppApp.swift
+//  HikesApp
+//
+//  Created by Yohan on 2024-10-11.
+//
+
+import SwiftUI
+
+@main
+struct HikesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
